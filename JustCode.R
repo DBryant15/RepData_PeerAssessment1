fb8819681ecfdb09ce7e56513ce544e4fd27fe3c
DerekBryant <dwbryan@nppd.com>
2018-04-10 13:49:53 -0500

    Another random save

--- JustCode.R
+++ JustCode.R
@@ -15,7 +15,7 @@ TotalDailySteps <- sqldf::sqldf("SELECT ConvertedToDate, sum(steps) as 'SumOfSte
 #20180410 1336 DWB having a lot of issue figuring out how to make a histogram 
 #of the daily steps
 #qplot(TotalDailySteps)
-#ggplot(TotalDailySteps,aes(x = TotalDailySteps$ConvertedToDate) +
- # stat_bin(binwidth=1, position="identity") + 
-  #scale_x_date(breaks=date_breaks(width="1 month"))
+#ggplot(TotalDailySteps,aes(x = TotalDailySteps$ConvertedToDate)) +
+#stat_bin(binwidth=1, position="identity") + 
+#scale_x_date(breaks=date_breaks(width="1 month"))
 #hist(TotalDailySteps, breaks = "days")
