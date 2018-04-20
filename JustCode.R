df5594d5b69ba60d581e6ffaebad3594e8efa85d
DerekBryant <dwbryan@nppd.com>
2018-04-20 16:58:17 -0500

    20180420 1657 DWB update with most recent content.

--- JustCode.R
+++ JustCode.R
@@ -2,20 +2,40 @@
 library("sqldf")
 library("ggplot2")
 library("lubridate")
-library("scales")
+#library("scales")
 
 ActTbl <- read.csv(unzip("activity.zip","activity.csv"), header = TRUE, stringsAsFactors = FALSE)
 ActTbl$ConvertedToDate <- ymd(ActTbl$date)
 
-#Question1 processing
+#Question1 - total steps per day
 TotalDailySteps <- sqldf::sqldf("SELECT ConvertedToDate, sum(steps) as 'SumOfSteps' 
              FROM ActTbl
-             WHERE steps <> 'NA'
-             GROUP BY date")
-#20180410 1336 DWB having a lot of issue figuring out how to make a histogram 
-#of the daily steps
-#qplot(TotalDailySteps)
-#ggplot(TotalDailySteps,aes(x = TotalDailySteps$ConvertedToDate)) +
-#stat_bin(binwidth=1, position="identity") + 
-#scale_x_date(breaks=date_breaks(width="1 month"))
-#hist(TotalDailySteps, breaks = "days")
+             WHERE steps IS NOT NULL
+             GROUP BY ConvertedToDate")
+
+hist(TotalDailySteps$SumOfSteps)
+mean(TotalDailySteps$SumOfSteps)
+median(TotalDailySteps$SumOfSteps)
+
+
+# Question 2 - Daily pattern
+AverageStepsInInterval <- sqldf::sqldf("SELECT interval, avg(steps) as 'AvgOfSteps' 
+            FROM ActTbl
+            WHERE steps IS NOT NULL
+            GROUP BY interval")
+
+dailyAvg_MaxSteps <- sqldf::sqldf("SELECT interval, max(AvgOfSteps) as 'MaxOfSteps' 
+            FROM AverageStepsInInterval")
+
+# Question 3 - Inputting missing values
+MissingStepsDataRows <- sqldf::sqldf("SELECT * 
+             FROM ActTbl
+             WHERE steps IS NULL
+             ") #GROUP BY ConvertedToDate
+
+CountOf_MissingStepsDataRows <- sqldf::sqldf("SELECT count(ConvertedToDate) 
+             FROM MissingStepsDataRows
+             ") #GROUP BY ConvertedToDate
+
+UPDATE_ActTbl <- sqpdf::sqldf("UPDATE ")
+
