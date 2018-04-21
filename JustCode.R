84efb2d08550f477a11c94b404efdcce555eea44
Derek <DBryant15>
2018-04-21 11:33:12 -0500

    20180421 1127 DWB Tried to get the update to work but couldnt seem to get it working. I did a simple test to just update the missing values to one. That failed also.
    Strangely when I tried to use sqldf() to open and then close the database (by just using call of sqldf()) as I had seen on a post it seemed to cause error. May need to explore this also.

--- JustCode.R
+++ JustCode.R
@@ -1,8 +1,13 @@
 #call in libraries
+
+
 library("sqldf")
 library("ggplot2")
 library("lubridate")
 #library("scales")
+
+#open connection to sqldf database
+#sqldf()
 
 ActTbl <- read.csv(unzip("activity.zip","activity.csv"), header = TRUE, stringsAsFactors = FALSE)
 ActTbl$ConvertedToDate <- ymd(ActTbl$date)
@@ -16,7 +21,6 @@ TotalDailySteps <- sqldf::sqldf("SELECT ConvertedToDate, sum(steps) as 'SumOfSte
 hist(TotalDailySteps$SumOfSteps)
 mean(TotalDailySteps$SumOfSteps)
 median(TotalDailySteps$SumOfSteps)
-
 
 # Question 2 - Daily pattern
 AverageStepsInInterval <- sqldf::sqldf("SELECT interval, avg(steps) as 'AvgOfSteps' 
@@ -37,5 +41,39 @@ CountOf_MissingStepsDataRows <- sqldf::sqldf("SELECT count(ConvertedToDate)
              FROM MissingStepsDataRows
              ") #GROUP BY ConvertedToDate
 
-UPDATE_ActTbl <- sqpdf::sqldf("UPDATE ")
+#create a new dataframe where the missing values are updated
+# with the average for the 5 minute interval
+
+#create copy of ActTbl to update
+UPDATED_ActTbl_missingAreIntervalAVG <- sqldf::sqldf("SELECT * FROM ActTbl")
+#20180421 1038 DWB - below failed with error: 
+#"Error in result_create(conn@ptr, statement) : near ".": syntax error"
+
+#moved away from this to try different methods...
+# sqldf::sqldf(c("UPDATE UPDATED_ActTbl_missingAreIntervalAVG 
+#              SET UPDATED_ActTbl_missingAreIntervalAVG.Interval = 
+# ((SELECT * FROM AverageStepsInInterval) as B) 
+#              WHERE UPDATED_ActTbl_missingAreIntervalAVG.Interval = B.Interval 
+#              AND UPDATED_ActTbl_missingAreIntervalAVG.steps IS NULL", 
+#              "SELECT * FROM main.UPDATED_ActTbl_missingAreIntervalAVG"), method = "raw")
+
+#20180421 1119 DWB accorfing to the following stack overflow 
+#SQLite may not be able to do inner join with updates:
+#https://stackoverflow.com/questions/19270259/update-with-join-in-sqlite
+
+#20180421 1045 DWB second attempt to replace NULL step values with 
+#the average in that interval
+#using no inner join in UPDATE
+#look to the post for an example that may work
+
+#test simple update
+UPDATED_ActTbl_missingAreIntervalAVG <- sqldf::sqldf(
+"UPDATE UPDATED_ActTbl_missingAreIntervalAVG  
+SET steps = 1 
+WHERE steps = 'NA'")
+
+#Question #4: Activity different pattern wkdy vs. wknd
+
+#Close sqldf connection
+#sqldf()
 
